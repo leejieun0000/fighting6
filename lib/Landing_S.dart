f58fe63f8fea8bac1c08514b6817a8a26f5a9409
DVUN <dhs9342@naver.com>
2023-05-10 18:05:42 +0900

    home and bottombar

---
+++ lib/Landing_S.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      title: 'landing_setting',
+      debugShowCheckedModeBanner: false,
+      home: LandingSetting(),
+    );
+  }
+}
+
+class LandingSetting extends StatefulWidget {
+  const LandingSetting({Key? key}) : super(key: key);
+
+  @override
+  _LandingSettingState createState() => _LandingSettingState();
+}
+
+class _LandingSettingState extends State<LandingSetting> {
+  DateTimeRange dateTimeRange = DateTimeRange(
+      start: DateTime.now(),
+      end: DateTime.now()
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final start = dateTimeRange.start;
+    final end = dateTimeRange.end;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('대여중',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.check),
+            color: Colors.black,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  SizedBox(width: 35,
+                  ),
+                  new Text(
+                      '물품 : ',
+                    style: TextStyle(fontSize: 20, color: Colors.black,),
+                  ),
+                  SizedBox(width: 20,),
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        isDense: true,
+                      ),
+                    ),
+                    width: 250,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  SizedBox(width: 35,
+                  ),
+                  new Text(
+                    '빌려주는 사람 : ',
+                    style: TextStyle(fontSize: 20, color: Colors.black,),
+                  ),
+                  SizedBox(width: 20,),
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        isDense: true,
+                      ),
+                    ),
+                    width: 200,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  SizedBox(width: 35,
+                  ),
+                  new Text(
+                    '빌리는 사람 : ',
+                    style: TextStyle(fontSize: 20, color: Colors.black,),
+                  ),
+                  SizedBox(width: 20,),
+                  Container(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        isDense: true,
+                      ),
+                    ),
+                    width: 200,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 30,),
+              // alignment: Alignment.centerLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '대여 날짜',
+                    style: TextStyle(fontSize: 20, color: Colors.black,),
+                  ),
+                  SizedBox(height: 10,),
+                  SizedBox(
+                    width: 320,
+                    height: 50,
+                    child: OutlinedButton(
+                        onPressed: () async {
+                          DateTimeRange? BorrowDate = await showDateRangePicker(
+                            context: context,
+                            initialDateRange: dateTimeRange,
+                            firstDate: DateTime(1900),
+                            lastDate: DateTime(2100),
+                            initialEntryMode: DatePickerEntryMode.calendarOnly,
+                            // locale: const Locale('ko', 'KR'),
+                          );
+                          if (BorrowDate != null){
+                            setState(() {
+                              dateTimeRange = BorrowDate;
+                            });
+                          }
+                        },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${start.year.toString()}-${start.month.toString().padLeft(2, '0')}-${start.day.toString().padLeft(2, '0')}',
+                            style: TextStyle(fontSize: 18, color: Colors.black,),
+                            textAlign: TextAlign.left,
+                          ),
+                          Icon(Icons.date_range),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Text(
+                    '반납 예정 날짜',
+                    style: TextStyle(fontSize: 20, color: Colors.black,),
+                  ),
+                  SizedBox(height: 10,),
+                  SizedBox(
+                    width: 320,
+                    height: 50,
+                    child: OutlinedButton(
+                      onPressed: () async {
+                        DateTimeRange? BorrowDate = await showDateRangePicker(
+                          context: context,
+                          initialDateRange: dateTimeRange,
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime(2100),
+                          initialEntryMode: DatePickerEntryMode.calendarOnly,
+                          // locale: const Locale('ko', 'KR'),
+                        );
+                        if (BorrowDate != null){
+                          setState(() {
+                            dateTimeRange = BorrowDate;
+                          });
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "${end.year.toString()}-${end.month.toString().padLeft(2, '0')}-${end.day.toString().padLeft(2, '0')}",
+                            style: TextStyle(fontSize: 18, color: Colors.black,),
+                            textAlign: TextAlign.left,
+                          ),
+                          Icon(Icons.date_range),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:project1/LB_page.dart';
+import 'package:project1/home_button.dart';
+
+class MyHome extends StatefulWidget{
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome>{
+  int _bottomSelectedIndex = 1;
+
+  void _tabSelect(int tabIndex){
+    setState(() {
+      _bottomSelectedIndex = tabIndex;
+    });
+  }
+
+  // List<Widget> _tapNavigatorBuilder(){
+  //   return [
+  //
+  //   ];
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text(
+          '자취생 도우미',
+          style: Theme.of(context).appBarTheme.titleTextStyle,
+        ),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.list),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.notifications),
+          ),
+        ],
+      ),
+      body: IndexedStack(
+        index: _bottomSelectedIndex,
+        children: [
+          Container(
+            color: Colors.accents[0],
+          ),
+          HomeButton(),
+          Container(
+            color: Colors.white,
+          ),
+          Container(
+            color: Colors.accents[10],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _bottomSelectedIndex,
+        onTap: (index){
+          setState(() {
+            _bottomSelectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: '알림'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+          label: 'home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.manage_accounts),
+            label: '설정'
+          ),
+        ],
+      ),
+    );
+  }
+}
