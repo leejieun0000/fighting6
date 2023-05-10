162c5a61e85650d24856750d7e10203216f68309
[Github <[Github>
2023-05-10 22:09:21 +0900

    hihi

--- lib/screen/groupPurchase_detail.dart
+++ lib/screen/groupPurchase_detail.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyWidget());
 }
@@ -62,7 +61,7 @@ class _MyWidget extends State<MyWidget> {
             ],
           ),
           body: Container(
-            margin: EdgeInsets.all(15),
+            margin: EdgeInsets.all(20),
             child: ListView(
               children: [
                 Padding(padding: EdgeInsets.all(4)),
@@ -70,14 +69,40 @@ class _MyWidget extends State<MyWidget> {
                   "트레이더스 공구 하실 분 구합니다.",
                   style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
                 ),
-                Image.asset(
-                  'images/flutter.png',
-                  width: 250.0,
-                  height: 250.0,
+                Padding(padding: EdgeInsets.all(4)),
+                Container(
+                  width: 380,
+                  height: 250,
+                  child:
+                  /*Image.asset(
+                          'image/emart.png',
+                          fit: BoxFit.fill,
+                          width: 380,
+                          height: 250,
+                        ),*/
+                  Container(
+                    width: 380,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: const Color(0xff60597B),
+                      image: new DecorationImage(
+                        fit: BoxFit.cover,
+                        colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                        image: AssetImage('image/emart.png'
+                        ),
+                      ),
+                    ),
+                    child: Text('마감',
+                        style: TextStyle(fontSize: 40),
+                        textAlign: TextAlign.center), //가운데 정렬이 안 먹힘... ^^
+                  ),
+
                 ),
+                Padding(padding: EdgeInsets.all(10)),
                 Container(
                   child: Text(
-                    "이마트 트레이더스에 파는 상품들이 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`",
+                    "이마트 트레이더스에 파는 상품들이 혼자 먹기에는 양이 너무 많아서 공구 하실 분을 찾습니다! "
+                        "같이 장 보러 가셔도 좋고, 사고 싶으신 물품을 저와 상의하셔도 좋습니다.",
                     style: TextStyle(fontSize: 20),
                   ),
                 ),
